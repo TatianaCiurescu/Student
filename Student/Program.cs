0906e970f3aeb6254ebb686d37e36c4c09eec1d7
TatianaSelaru <tatts.selaru@gmail.com>
2022-01-02 23:09:37 +0200

    student

--- Student/Program.cs
+++ Student/Program.cs
@@ -6,7 +6,7 @@ namespace Homework2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please, write your full name: ");
+            /*Console.WriteLine("Please, write your full name: ");
             string name = Console.ReadLine();
 
             Console.WriteLine("Please, write your age: ");
@@ -14,11 +14,40 @@ namespace Homework2
 
             Student student = new Student(name, age);
 
-            Console.WriteLine(student.Info);
+            Console.WriteLine(student.Info);*/
+            Bonus();
+        }
 
+        static void Bonus()
+        {
+            Console.WriteLine("Enter the number of students: ");
+            int length = Int32.Parse(Console.ReadLine());
+            Student[] students = new Student[length];
 
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("Please, write full name: ");
+                string name = Console.ReadLine();
 
-           
+                Console.WriteLine("Please, write age: ");
+                int age = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please, write mark: ");
+                int mark = Int32.Parse(Console.ReadLine());
+
+                students[i] = new Student(name, age);
+                students[i].Mark = mark;
+            }
+
+            int sum = 0;
+
+            foreach (var student in students)
+            {
+                if (student.Mark.HasValue)
+                    sum += student.Mark.Value;
+            }
+
+            Console.WriteLine($"The average mark is: {sum / length}");
 
         }
     }
